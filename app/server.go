f702e75e3aba93a6cc87c6fbe6347a9938ea23df
長濱拓哉 <nagahamatakuya@nagahamatakuyanoMacBook-Pro-2.local>
2021-09-25 15:49:02 +0900

    chore: fix go data

--- app/server.go
+++ app/server.go
@@ -3,13 +3,27 @@ package main
 import (
 	"net/http"
 
+	"github.com/go-playground/validator/v10"
 	"github.com/labstack/echo/v4"
+
+	"app/handler"
 )
+
+type CustomValidator struct {
+    validator *validator.Validate
+}
+
+func (cv *CustomValidator) Validate(i interface{}) error {
+	if err := cv.validator.Struct(i); err != nil {
+	  return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+	return nil
+}
 
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "!!!Hello, World!!!")
-	})
+	e.Validator = &CustomValidator{validator: validator.New()}
+	e.GET("/", handler.Hello)
+	e.POST("/login", handler.Login)
 	e.Logger.Fatal(e.Start(":4000"))
 }
