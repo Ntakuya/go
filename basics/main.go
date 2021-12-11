6f4160dffd3e41518a5b6d7326a17d98d5d6970a
長濱拓哉 <nagahamatakuya@nagahamatakuyanoMacBook-Pro-2.local>
2021-12-11 15:55:24 +0900

    feat(go/tutorial.basics) add nil slice

--- basics/main.go
+++ basics/main.go
@@ -5,5 +5,5 @@ import (
 )
 
 func main() {
-  sample.SliceLenCap()
+  sample.PrinNilSlices()
 }
---
+++ basics/sample/nil-slices.go
@@ -0,0 +1,19 @@
+package sample
+
+import "fmt"
+
+func PrinNilSlices() {
+  var s []int
+  m := make([]int, 0)
+  // var a [0]int
+  printSliceWithCapAndLength(s)
+  printSliceWithCapAndLength(m)
+  // printSliceWithCapAndLength(a)
+}
+
+func printSliceWithCapAndLength(s []int) {
+  fmt.Println(s, cap(s), len(s))
+  if s == nil {
+    fmt.Println("NILL!!!")
+  }
+}
