c3eafe37f5d459b4c74dfd4471eeb0a13aaffd07
長濱拓哉 <nagahamatakuya@nagahamatakuyanoMacBook-Pro-2.local>
2021-12-11 15:02:23 +0900

    feat(tutorial/basics) add pointer study

--- basics/main.go
+++ basics/main.go
@@ -2,17 +2,11 @@ package main
 
 import (
 	"basics/sample"
-	"fmt"
 )
 
 func main() {
-  fmt.Println(sample.ForIsGosWhile())
-  fmt.Println(sample.Sqrt(2), sample.Sqrt(-4))
-  fmt.Println(sample.Pow(3,2,10), sample.Pow(3, 3, 20))
-  fmt.Println(sample.PowWithElse(3,2,10), sample.PowWithElse(3, 3, 20))
-  fmt.Println(sample.SwitchSample())
-  fmt.Println(sample.SwitchEvaluationOrder())
-  fmt.Println(sample.SwitchWithNoCondition())
-  fmt.Println(sample.Defer())
-  sample.DeferMulti()
+  sample.CallVertex()
+  sample.CallVertexX()
+  sample.CallWithPointer()
+  sample.CallWithDefference()
 }
---
+++ basics/sample/pointer.go
@@ -0,0 +1,26 @@
+package sample
+
+import "fmt"
+
+type pointerMethods struct {
+}
+
+func NewPointerMethod() *pointerMethods {
+  return &pointerMethods{}
+}
+
+func (ps *pointerMethods) Pointer() {
+  i, j := 42, 2701
+  fmt.Println(i) // value
+  fmt.Println(&i)// pointer
+
+  p := &i // p is pointer of i
+  fmt.Println(*p)// the value of pointer
+
+  *p = 21 // insert value into p of pointer
+  fmt.Println("*p is the value of p so, *p is", *p)
+  fmt.Println("*p is the value of p so, p is", p)
+  p = &j
+  *p = *p / 37
+  fmt.Println(j)
+}
