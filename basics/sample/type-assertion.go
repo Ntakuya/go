573ad05ce7bd24a48be82df220ecd4bc34a3349e
長濱拓哉 <nagahamatakuya@nagahamatakuyanoMacBook-Pro-2.local>
2021-12-13 22:07:42 +0900

    feat(go/tutorial.basic) add type assertin

---
+++ basics/sample/type-assertion.go
@@ -0,0 +1,16 @@
+package sample
+
+import "fmt"
+
+func CallTypeAssertion() {
+  var i interface{} = "hello"
+
+	s := i.(string)
+	fmt.Println(s)
+
+	s, ok := i.(string)
+	fmt.Println(s, ok)
+
+	f, ok := i.(float64)
+	fmt.Println(f, ok)
+}
